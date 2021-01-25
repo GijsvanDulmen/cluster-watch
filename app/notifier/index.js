f9f7a1838881977af418c367b902aeff5fda35ea
Gijs van Dulmen <gijs@vandulmen.net>
2021-01-25 20:50:47 +0100

    Add roles and vanilla kubernetes support

--- app/notifier/index.js
+++ app/notifier/index.js
@@ -32,11 +32,21 @@ if ( process.env.KUBERNETES_SERVICE_HOST ) {
     kc.loadFromDefault();
 }
 
+// check for openshift
+let enableOpenshift = true;
+console.log(process.env.DISABLE_OPENSHIFT);
+if ( process.env.DISABLE_OPENSHIFT != undefined
+    && process.env.DISABLE_OPENSHIFT == "true") {
+    enableOpenshift = false;
+}
+logger.info("openshift support enabled: " + enableOpenshift);
+
 // setup slack bot
 const bot = new SlackBot(process.env.SLACK_CHANNEL, process.env.SLACK_TOKEN, logger);
 
 const cache = {};
 const taintTimeCache = {};
+const nodeRoleCache = {};
 
 const taintsToWatch = {
     NotReady: 'node.kubernetes.io/not-ready',
@@ -61,12 +71,13 @@ const icons = {
     OK: ":white_check_mark:",
     INFO: ":building_construction:",
     QUESTION: ":question:",
-    HOURGLASS: ":hourglass:"
+    HOURGLASS: ":hourglass:",
+    WAVE: ":wave:"
 };
 
 const intervals = {
     API_RESPONSE_TIMES: 1000*60*5,
-    NODE_CHECK: 1000*60*5,
+    NODE_CHECK: 1000*5,
     WATCHDOG: 1000*60*60,
     STILL_TAINTED: 1000*60*5,
 };
@@ -77,53 +88,55 @@ setInterval(() => {
 }, intervals.WATCHDOG);
 
 // machine config pool
-const mcp = new MachineConfigPoolWatcher(kc, logger);
-const mcpCache = {};
-const statusFieldsToMonitor = [
-    "degradedMachineCount",
-    "machineCount",
-    "observedGeneration",
-    "readyMachineCount",
-    "unavailableMachineCount",
-    "updatedMachineCount"
-];
+if ( enableOpenshift ) {
+    const mcp = new MachineConfigPoolWatcher(kc, logger);
+    const mcpCache = {};
+    const statusFieldsToMonitor = [
+        "degradedMachineCount",
+        "machineCount",
+        "observedGeneration",
+        "readyMachineCount",
+        "unavailableMachineCount",
+        "updatedMachineCount"
+    ];
 
-mcp.onInit(obj => {
-    const name = obj.metadata.name;
-    mcpCache[name] = {};
-    statusFieldsToMonitor.forEach(field => {
-        mcpCache[name][field] = obj.status[field];
-    });
-    logger.info(name + ": " + JSON.stringify(mcpCache[name]));
-});
-mcp.onUpdate(obj => {
-    const name = obj.metadata.name;
-    if ( mcpCache[name] == undefined ) {
+    mcp.onInit(obj => {
+        const name = obj.metadata.name;
         mcpCache[name] = {};
-    }
-
-    statusFieldsToMonitor.forEach(field => {
-        if ( mcpCache[name][field] != undefined ) {
-            if ( mcpCache[name][field] != obj.status[field] ) {
-                bot.send("MachineConfigPool `"+name+"` field `"+field+"` changed from `"+mcpCache[name][field]+"` to `"+obj.status[field]+"`", colors.YELLOW, icons.INFO);
-            }
-        }
-        mcpCache[name][field] = obj.status[field];
+        statusFieldsToMonitor.forEach(field => {
+            mcpCache[name][field] = obj.status[field];
+        });
+        logger.info(name + ": " + JSON.stringify(mcpCache[name]));
     });
-});
-mcp.setup();
+    mcp.onUpdate(obj => {
+        const name = obj.metadata.name;
+        if ( mcpCache[name] == undefined ) {
+            mcpCache[name] = {};
+        }
 
-// machine config watcher
-const mc = new MachineConfigWatcher(kc, logger);
-mc.onCreate(obj => {
-    bot.send("New MachineConfig `"+obj.metadata.name+"` available", colors.YELLOW, icons.INFO);
-});
-mc.setup();
+        statusFieldsToMonitor.forEach(field => {
+            if ( mcpCache[name][field] != undefined ) {
+                if ( mcpCache[name][field] != obj.status[field] ) {
+                    bot.send("MachineConfigPool `"+name+"` field `"+field+"` changed from `"+mcpCache[name][field]+"` to `"+obj.status[field]+"`", colors.YELLOW, icons.INFO);
+                }
+            }
+            mcpCache[name][field] = obj.status[field];
+        });
+    });
+    mcp.setup();
+
+    // machine config watcher
+    const mc = new MachineConfigWatcher(kc, logger);
+    mc.onCreate(obj => {
+        bot.send("New MachineConfig `"+obj.metadata.name+"` available", colors.YELLOW, icons.INFO);
+    });
+    mc.setup();
+}
 
 // namespaces
 const ns = new NamespaceWatcher(kc, logger, 1000*5*60);
 ns.onCreate(obj => bot.send("namespace created: `" + obj.metadata.name + "`", colors.BLUE, icons.INFO));
-ns.onDelete(name => bot.send("namespace deleted: `" + name + "`", colors.RED, icons.ERROR));
+ns.onDelete(name => bot.send("namespace deleted: `" + name + "`", colors.BLUE, icons.WAVE));
 ns.setup();
 
 // api server response time
@@ -138,9 +151,37 @@ const checkApiServerResponse = () => {
 setInterval(() => checkApiServerResponse(), intervals.API_RESPONSE_TIMES);
 checkApiServerResponse();
 
+const updateNodeRoles = obj => {
+    const name = obj.metadata.name;
+
+    nodeRoleCache[name] = [];
+
+    // update role
+    if ( obj.metadata.labels
+        && obj.metadata.labels ) {
+        Object.keys(obj.metadata.labels).forEach(label => {
+            const searchStr = "node-role.kubernetes.io/";
+            if ( label.indexOf(searchStr) != -1 ) {
+                nodeRoleCache[name].push(label.substr(searchStr.length));
+            }
+        });
+    }
+};
+
+const getRoleNodeStr = name => {
+    let nodeRoleString = '';
+    if ( nodeRoleCache[name] && nodeRoleCache[name].length > 0 ) {
+        nodeRoleString = ' roles `'+nodeRoleCache[name].join('`/`')+'`';
+    }
+    console.log(nodeRoleString);
+    return nodeRoleString;
+};
+
 const checkTaints = obj => {
     const name = obj.metadata.name;
     const spec = obj.spec;
+
+    updateNodeRoles(obj);
 
     // check taints
     let currentTaints = {};
@@ -169,16 +210,16 @@ const checkTaints = obj => {
                 if ( currentTaints[taintKey] ) {
                     // add time as a marker
                     taintTimeCache[name][taintKey] = setInterval(() => {
-                        bot.send('Node `' + name + "` still has the taint `" + taintKey + "`", colors.RED, icons.QUESTION);
+                        bot.send('Node `' + name + "`"+getRoleNodeStr(name)+" still has the taint `" + taintKey + "`", colors.RED, icons.QUESTION);
                     }, intervals.STILL_TAINTED);
 
-                    bot.send('Node `' + name + "` has the taint `" + taintKey + "`", colors.RED, icons.ERROR);
+                    bot.send('Node `' + name + "`"+getRoleNodeStr(name)+" tainted `" + taintKey + "`", colors.RED, icons.ERROR);
                 } else {
                     if ( taintTimeCache[name][taintKey] != undefined ) {
                         clearInterval(taintTimeCache[name][taintKey]);
                         delete taintTimeCache[name][taintKey];
                     }
-                    bot.send('Node `' + name + "` has no taint `" + taintKey + "` again!", colors.GREEN, icons.OK);
+                    bot.send('Node `' + name + "`"+getRoleNodeStr(name)+" untainted `" + taintKey + "` again!", colors.GREEN, icons.OK);
                 }
             }
         });
@@ -191,16 +232,25 @@ const checkTaints = obj => {
 // watch nodes
 const n = new NodeWatcher(kc, logger, intervals.NODE_CHECK);
 n.onDelete(name => {
+    bot.send('Node `' + name + "` " + getRoleNodeStr(name) + " deleted", colors.YELLOW, icons.WAVE)
+
     Object.keys(taintTimeCache[name]).forEach(taintKey => {
         clearInterval(taintTimeCache[name][taintKey]);
     });
     delete taintTimeCache[name];
+    delete nodeRoleCache[name];
 });
 n.onCreate(obj => {
-    bot.send('Node `' + obj.metadata.name + "` is added", colors.YELLOW, icons.INFO)
+    updateNodeRoles(obj);
+    
+    bot.send('Node `' + obj.metadata.name + "` added" + getRoleNodeStr(obj.metadata.name), colors.YELLOW, icons.INFO)
     checkTaints(obj);
 });
-n.onUpdate(obj => checkTaints(obj));
+n.onInit(obj => updateNodeRoles(obj));
+n.onUpdate(obj => {
+    updateNodeRoles(obj);
+    checkTaints(obj);
+});
 n.setup();
 
 app.get('/live', (req, res) => res.send("OK")); // live and readiness probe
